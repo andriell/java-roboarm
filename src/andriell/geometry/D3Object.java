7b2c1030d95ea0bf3c4c496eaf127e03755e6921
andriell <root.andriell@gmail.com>
2018-03-03 10:47:50 +0300

    start

--- src/andriell/geometry/D3Object.java
+++ src/andriell/geometry/D3Object.java
@@ -5,17 +5,17 @@ package andriell.geometry;
  */
 public class D3Object {
     private final int l = 3;
-    protected double line[][] = new double[0][];
+    protected double points[][] = new double[0][];
     private double[] translation = new double[]{0d, 0d, 0d};
     private double[] rotation = new double[]{0d, 0d, 0d};
     private double size = 1d;
     private double[] cos = {1d, 1d, 1d};
     private double[] sin = {1d, 1d, 1d};
 
-    public double[][] getLines() {
-        double[][] r = new double[line.length][];
-        for (int i = 0; i < line.length; i++) {
-            r[i] = calculatePoint(line[i]);
+    public double[][] getPoints() {
+        double[][] r = new double[points.length][];
+        for (int i = 0; i < points.length; i++) {
+            r[i] = calculatePoint(points[i]);
         }
         return r;
     }
@@ -23,20 +23,27 @@ public class D3Object {
     protected double[] calculatePoint(double[] p) {
         double[] r = new double[l];
         r[0] = size * (translation[0] + p[0] * cos[1] * cos[2] - p[2] * sin[1] - p[1] * cos[1] * sin[2]);
-        r[1] = size * (translation[1] - p[2] * cos[1] * sin[0] + p[0] * ( cos[0] * sin[2] - cos[2] * sin[0] * sin[1]) + p[1] * (cos[0] * cos[2] + sin[0] * sin[1] * sin[2]));
+        r[1] = size * (translation[1] - p[2] * cos[1] * sin[0] + p[0] * (cos[0] * sin[2] - cos[2] * sin[0] * sin[1]) + p[1] * (cos[0] * cos[2] + sin[0] * sin[1] * sin[2]));
         r[2] = size * (translation[2] + p[2] * cos[0] * cos[1] + p[0] * (cos[0] * cos[2] * sin[1] + sin[0] * sin[2]) + p[1] * (cos[2] * sin[0] - cos[0] * sin[1] * sin[2]));
         return r;
     }
 
-    public void setMaxLines(int s) {
-        line = new double[s][];
+    public void setMaxPoints(int s) {
+        points = new double[s][];
         for (int i = 0; i < s; i++) {
-            line[i] = new double[3];
+            points[i] = new double[3];
         }
     }
 
-    public int getMaxLines(int s) {
-        return line.length;
+    public int getMaxPoints(int s) {
+        return points.length;
+    }
+
+    public void setPoint(int i, double[] xyz) {
+        if (i < 0 || i > points.length || xyz == null || xyz.length != 3) {
+            return;
+        }
+        points[i] = xyz;
     }
 
     public double getSize() {
@@ -66,8 +73,17 @@ public class D3Object {
             return;
         }
         rotation[i] = v;
-        cos[i] = Math.cos(v);
-        sin[i] = Math.sin(v);
+        cos[i] = Math.cos(rotation[i]);
+        sin[i] = Math.sin(rotation[i]);
+    }
+
+    public void setRotationAngle(int i, double v) {
+        if (i < 0 || i > l) {
+            return;
+        }
+        rotation[i] = (Math.PI * v) / 180d;
+        cos[i] = Math.cos(rotation[i]);
+        sin[i] = Math.sin(rotation[i]);
     }
 
     public double getRotation(int i) {
--- src/andriell/robo/arm/Arm.java
+++ src/andriell/robo/arm/Arm.java
@@ -1,12 +1,14 @@
 package andriell.robo.arm;
 
+import andriell.geometry.D3Object;
 import andriell.robo.arm.gui.ImagePane;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class Arm implements ImagePane.Entity {
+public class Arm extends D3Object implements ImagePane.Entity {
     private BufferedImage image;
+    private final double armSize = 500;
     private Stroke stroke1 = new BasicStroke(1);
     private Stroke stroke2 = new BasicStroke(2);
     private Color[] edgesColor = new Color[] {
@@ -23,26 +25,19 @@ public class Arm implements ImagePane.Entity {
 
     public Arm() {
         image = new BufferedImage(WH[0], WH[1], BufferedImage.TYPE_4BYTE_ABGR);
+        setMaxPoints(3);
+        setRotationAngle(0, 45);
+        setRotationAngle(1, 45);
+        setRotationAngle(2, 45);
     }
 
-    public double getA1() {
-        return a1;
-    }
 
     public void setA1(double a1) {
         this.a1 = a1;
     }
 
-    public double getA2() {
-        return a2;
-    }
-
     public void setA2(double a2) {
         this.a2 = a2;
-    }
-
-    public double getA3() {
-        return a3;
     }
 
     public void setA3(double a3) {
@@ -71,14 +66,20 @@ public class Arm implements ImagePane.Entity {
         g.drawString("A2 " + Math.round(a2), 5, 30);
         g.drawString("A3 " + Math.round(a3), 5, 45);
 
-        g.setColor(Color.black);
-        g.drawLine(0, WH[1] / 2, WH[0], WH[1] / 2);
-        g.drawLine(WH[0] / 2, 0, WH[0] / 2, WH[1]);
-
         g.setStroke(stroke2);
 
-        g.setColor(edgesColor[0]);
-        g.drawLine(0, 0, 100, 100);
+        points[0][0] = 0d;
+        points[0][1] = 0d;
+        points[0][2] = 0d;
+
+        points[1][0] = armSize * Math.cos(Math.PI / 180 * a1);
+        points[1][1] = armSize * Math.sin(Math.PI / 180 * a1);
+        points[1][2] = armSize * Math.sin(Math.PI / 180 * a2);
+
+        double newPoints[][] = getPoints();
+
+        g.setColor(Color.red);
+        g.drawLine((int) newPoints[0][0], (int) newPoints[0][1], (int) newPoints[1][0], (int) newPoints[1][1]);
 
 
     }
